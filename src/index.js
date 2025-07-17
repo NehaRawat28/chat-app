d80193c9f35147ad4e66fc6fff139308349ecf0c
Neha Rawat <nr.neha9990@gmail.com>
2025-07-17 16:33:32 +0530

    Made updates

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
-import './index.css';
-import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-import setupSocket from './sockets'
-import reducers from './reducers'
-import handleNewMessage from './sagas'
-import username from './utils/name'
+import App from "./App";
+import reducers from "./reducers";
 
-const sagaMiddleware = createSagaMiddleware()
+import setupSocket from "./sockets";
+import handleNewMessage from "./sagas";
+import username from "./utils/name";
 
+// ✅ 1. create saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// ✅ 2. create store with middleware
 const store = createStore(
-	reducers,
-	applyMiddleware(sagaMiddleware)
-)
+  reducers,
+  applyMiddleware(sagaMiddleware)
+);
 
-const socket = setupSocket(store.dispatch, username)
+// ✅ 3. setup WebSocket AFTER store is created
+const socket = setupSocket(store.dispatch, username);
 
-sagaMiddleware.run(handleNewMessage, {socket, username})
+// ✅ 4. run saga AFTER socket is ready
+sagaMiddleware.run(handleNewMessage, { socket, username });
 
-ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>, 
-	document.getElementById('root')
-	);
-//registerServiceWorker();
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
